11a3a9c04393ce3b1f54cb537a0b491100f9aa6d
Kay McKelly <frecklestars@gmail.com>
2017-04-11 16:16:57 -0700

    More style removal

--- staticModule/script.js
+++ staticModule/script.js
@@ -1,14 +1,9 @@
-(function (angular) {
-  'use strict';
+// declare a module
+var myAppModule = angular.module('myApp', []);
 
-  // declare a module
-  var myAppModule = angular.module('myApp', []);
-
-  // configure the module
-  myAppModule.filter('greet', function() {
-    return function(name) {
-      return 'Hello ' + name + '!';
-    };
-  });
-
-})(window.angular);
+// configure the module
+myAppModule.filter('greet', function() {
+  return function(name) {
+    return 'Hello ' + name + '!';
+  };
+});
