6bdae6f2582f7c243190a5d60f262204c20093d4
Kay McKelly <frecklestars@gmail.com>
2017-04-11 17:00:49 -0700

    Update images, todo list

--- toDoList/script.js
+++ toDoList/script.js
@@ -1,16 +1,22 @@
+// declaring module and TodoList controller
 angular.module('todoApp', [])
   .controller('TodoListController', function() {
     var todoList = this;
+
+    // sets up initial items on todo list
     todoList.todos = [
       {text:'learn AngularJS', done:true},
       {text:'build Angular app', done:false},
-      {text:'profit', done:false}];
+      {text:'profit', done:false}
+    ];
 
+    // function that executes when you add an item
     todoList.addTodo = function() {
       todoList.todos.push({text:todoList.todoText, done:false});
       todoList.todoText = '';
     };
 
+    // setting up the counter
     todoList.remaining = function() {
       var count = 0;
       angular.forEach(todoList.todos, function(todo) {
@@ -19,6 +25,7 @@ angular.module('todoApp', [])
       return count;
     };
 
+    // function that executes to remove items
     todoList.archive = function() {
       var oldTodos = todoList.todos;
       todoList.todos = [];
